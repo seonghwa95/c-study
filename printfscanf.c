e2071ae3e4b821b15152276b9821b100418fec96
seonghwa95 <holyflower95@naver.com>
2024-03-10 23:57:30 +0900

    pringf sacnf

---
+++ printfscanf.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+int main(void)
+{
+    // 주석을 다는 방법
+
+    /*
+     * 이렇게 범위 주석도 가능
+     */
+
+    // (정수형) 변수 할당
+    int age = 29;
+    printf("%d\n", age);
+
+    // 변수 값 변경
+    age = 30;
+    printf("%d\n", age);
+
+    return 0;
+}
